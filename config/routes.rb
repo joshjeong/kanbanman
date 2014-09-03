9441e002facf1ca01d6d7ab3dc27468ec5170adf
Josh Jeong <jcjeong@gmail.com>
2014-09-03 14:34:17 -0700

    Changes routes to match controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :user do
+  resources :users do
     resources :tasks, except: :show
   end
 
-  root 'user#index'
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
